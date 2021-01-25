6c0d60dbf34c3244046827366e7bb162376f7490
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-01-25 10:36:05 +0100

    yeet

---
+++ src/database/index.js
@@ -0,0 +1,41 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const Posts = require("./posts");
+const Profile = require("./profile");
+const EXPIER = require("./exp");
+
+const sequelize = new Sequelize( //AUTHORIIZES AND STARTS SEQUELIZE
+    process.env.PGDATABASE, //DATABASE NAME
+    process.env.PGUSER, //PG USERNAME
+    process.env.PGPASSWORD, //PG PASSWORD
+    {
+        host: process.env.PGHOST, //HOST NAME
+        dialect: "postgres", //THIS IS THE LANGUAGE THAT WE ARE USING WITH SEQUELIZE
+    }
+);
+
+const models = {
+    //OBJECT CONTAINING MODELS OF THE TABLES
+    Posts: Posts(sequelize, DataTypes),
+    EXPIER: EXPIER(sequelize, DataTypes),
+    Profile: Profile(sequelize, DataTypes),
+};
+
+//GOES THROUGH EACH KEY OF THE MODEL OBJECT ABOVE (e.g. Author: Author(sequelize, DataTypes)) AND CREATES TABLE RELATIONS DEPENDING ON CODE INSIDE EACH MODEL
+Object.keys(models).forEach((modelName) => {
+    if ("associate" in models[modelName]) {
+        //IF THE WORD ASSOCIATE IS PRESENT INSIDE THE MODEL, IT WILL RUN THE FOLLOWING CODE
+        models[modelName].associate(models); //RUNS THE .associate INSIDE THE MODEL, WHICH IS CODE THAT DEFINES THE TABLES RELATIONS
+    }
+});
+
+models.sequelize = sequelize; //PUTS TABLE MODELS INTO SEQUELIZE
+models.Sequelize = Sequelize; //IDK WHAT THIS DOES :(
+
+sequelize
+    .authenticate() //TESTS IF SEQUELIZE IS CONNECTED TO OUR DATABASE
+    .then(() => console.log("Connected"))
+    .catch((e) => {
+        console.log("not connected"), console.log(e);
+    });
+
+module.exports = models; //EXPORTS SEQUELIZE CONTAINING TABLE MODELS TO SERVER.JS
---
+++ src/database/posts.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+    const Posts = sequelize.define(
+        "Posts",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            text: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+
+            imgurl: {
+                type: DataTypes.STRING(500),
+                allowNull: false,
+            },
+        },
+        { timestamps: true }
+    );
+    Posts.associate = (models) => {
+        Posts.belongsTo(models.Profile); //ONE-TO-MANY: REVIEW IS MANY, ARTICLE IS ONE
+    };
+    return Posts;
+};
