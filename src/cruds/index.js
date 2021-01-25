47de962ad9c85aeb18981f247465481485b688d7
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-01-25 18:25:59 +0100

    forgot to add route

--- src/cruds/index.js
+++ src/cruds/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const profileRoute = require("./profile");
 const postRoute = require("./posts");
 const expRoute = require("./exp");
+const commentRoute = require("./comments");
 
 router.use("/profile", profileRoute);
 router.use("/posts", postRoute);
 router.use("/exp", expRoute);
+router.use("/comments", commentRoute);
 module.exports = router;
